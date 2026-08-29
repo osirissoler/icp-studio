22548122850a50e81d8e5d6a89e766d63cf644cb
Osiris Soler Ramírez <35948070+osirissoler@users.noreply.github.com>
2026-08-29 13:20:27 -0400

    style: format projector route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -11,9 +11,9 @@ const routes: RouteRecordRaw[] = [
   },
 
   {
-  path: '/projector',
-  component: () => import('@/pages/ProjectorPage.vue'),
-},
+    path: '/projector',
+    component: () => import('@/pages/ProjectorPage.vue'),
+  },
 
   // Always leave this as last one,
   // but you can also remove it
