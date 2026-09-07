38d2ecc674c64611b56da1d045d3cfc744313c85
Osiris Soler Ramírez <35948070+osirissoler@users.noreply.github.com>
2026-09-07 03:45:25 -0400

    feat: install live tool projection destination portal

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -2,32 +2,26 @@ import { defineBoot } from '#q-app';
 import { createI18n } from 'vue-i18n';
 
 import messages from '@/i18n';
+import { installProjectionDestinationPortal } from '../services/projection-destination-portal';
 
 export type MessageLanguages = keyof typeof messages;
-// Type-define 'en-US' as the master schema for the resource
 export type MessageSchema = (typeof messages)['en-US'];
 
-// See https://vue-i18n.intlify.dev/guide/advanced/typescript.html#global-resource-schema-type-definition
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 declare module 'vue-i18n' {
-  // define the locale messages schema
   export interface DefineLocaleMessage extends MessageSchema {}
-
-  // define the datetime format schema
   export interface DefineDateTimeFormat {}
-
-  // define the number format schema
   export interface DefineNumberFormat {}
 }
 /* eslint-enable @typescript-eslint/no-empty-object-type */
 
-export default defineBoot(({ app }) => {
+export default defineBoot(({ app, router }) => {
   const i18n = createI18n<{ message: MessageSchema }, MessageLanguages>({
     locale: 'en-US',
     legacy: false,
     messages,
   });
 
-  // Set i18n instance on app
   app.use(i18n);
+  installProjectionDestinationPortal(router);
 });
